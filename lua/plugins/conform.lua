86ff8e0582154031a93583e76b9f141e25c2d518
MoyenMed <dadelidela@gmail.com>
2024-08-25 19:27:19 -0400

    Config changes for formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -9,12 +9,20 @@ return {
         cpp = { "clang-format" },
         lua = { "stylua" },
       },
-      format_on_save = {
-        lsp_fallback = true, -- use lsp for formatting if no specific formatter
-        async = false, -- non asynchronous mapping (so synchronous?)
-        timeout_ms = 500, -- fails if formatting takes more than 500 ms
-      },
+      format_on_save = function(bufnr)
+        -- Disable formatting on save if certain properties are set
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return {
+          lsp_fallback = true, -- use lsp for formatting if no specific formatter
+          async = false, -- non asynchronous mapping (so synchronous?)
+          timeout_ms = 500, -- fails if formatting takes more than 500 ms
+        }
+      end,
     })
+    -- disable auto formatting on save
+    vim.g.disable_autoformat = true
 
     -- keymap for formatting
     vim.keymap.set({ "n", "v" }, "<leader>fo", function()
